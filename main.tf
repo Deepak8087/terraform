b5e1910eeda61af81d2c5b704db4de5967a87d2b
Deepak8087 <Chavandeepak2011@gmail.com>
2025-07-04 11:59:16 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "one" {
+  ami           = "ami-040361ed8686a66a2"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "raham-server"
+  }
+}
